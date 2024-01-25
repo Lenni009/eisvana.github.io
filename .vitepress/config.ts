4c64f633e362a76cf06c5bf90eb254d18620cc30
Lenni009 <guentherhanslphdyt@gmail.com>
2024-01-25 09:02:22 +0100

    favicon adjustments

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -9,12 +9,15 @@ export default defineConfig({
   srcDir: 'pages',
   cleanUrls: true,
   head: [
-    ['link', { rel: 'icon', sizes: '128x128', href: '/icons/favicon-128x128.png' }],
-    ['link', { rel: 'icon', sizes: '96x96', href: '/icons/icons/favicon-96x96.png' }],
-    ['link', { rel: 'icon', sizes: '32x32', href: '/icons/favicon-32x32.png' }],
-    ['link', { rel: 'icon', sizes: '16x16', href: '/icons/favicon-16x16.png' }],
-    ['link', { rel: 'icon', href: '/icons/favicon.ico' }],
-    ['link', {rel: 'stylesheet', href: '/globalStyle.css'}],
+    ['meta', { name: 'theme-color', content: '#280033' }],
+    ['meta', { name: 'msapplication-config', content: '/icons/browserconfig.xml' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#280033' }],
+    ['link', { rel: 'manifest', href: '/icons/site.webmanifest' }],
+    ['link', { rel: 'icon', sizes: '180x180', href: '/icons/apple-touch-icon.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/icons/favicon-32x32.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/icons/favicon-16x16.png' }],
+    ['link', { rel: 'shortcut icon', href: '/favicon.ico' }],
+    ['link', { rel: 'stylesheet', href: '/globalStyle.css' }],
   ],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
