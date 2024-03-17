16b9b5e1330087504d023dab84c7b094005a5870
Lenni009 <guentherhanslphdyt@gmail.com>
2024-03-17 20:18:36 +0100

    fix build

--- variables/members.ts
+++ variables/members.ts
@@ -1,10 +1,9 @@
 import type { EisvanaMember } from '../types/member';
 import { isEisvanaMember, isObject } from '../types/assertion';
 
-const members = import.meta.glob('./members/*.ts');
-const memberImports = Object.values(members);
-const memberPromises = memberImports.map(async (memberFunction) => memberFunction());
-const memberItems = await Promise.all(memberPromises);
+// using `eager` to not get promises, since top-level await throws errors for some reason (not implemented in target browsers, despite target being ESNext)
+const members = import.meta.glob('./members/*.ts', {eager: true});
+const memberItems = Object.values(members);
 const wellFormedMembers = memberItems
   .map((obj) => {
     if (isObject(obj) && 'default' in obj) return obj.default;
