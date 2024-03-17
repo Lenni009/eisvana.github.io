103b40441f99fb1d3bae7766b6685ee9ea139f07
Lenni009 <guentherhanslphdyt@gmail.com>
2024-03-17 20:21:32 +0100

    import only default export

--- variables/members.ts
+++ variables/members.ts
@@ -2,13 +2,8 @@ import type { EisvanaMember } from '../types/member';
 import { isEisvanaMember, isObject } from '../types/assertion';
 
 // using `eager` to not get promises, since top-level await throws errors for some reason (not implemented in target browsers, despite target being ESNext)
-const members = import.meta.glob('./members/*.ts', { eager: true });
+const members = import.meta.glob('./members/*.ts', { eager: true, import: 'default' });
 const memberItems = Object.values(members);
-const wellFormedMembers = memberItems
-  .map((obj) => {
-    if (isObject(obj) && 'default' in obj) return obj.default;
-    return undefined;
-  })
-  .filter(isEisvanaMember);
+const wellFormedMembers = memberItems.filter(isEisvanaMember);
 
 export const eisvanaMembers: EisvanaMember[] = wellFormedMembers;
