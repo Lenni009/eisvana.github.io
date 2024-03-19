35e84d9cb0c5c0c8379034f1fb7df22fbb832b8c
Lenni009 <guentherhanslphdyt@gmail.com>
2024-03-19 14:23:06 +0100

    compress to webp

--- logic/compressImage.ts
+++ logic/compressImage.ts
@@ -5,13 +5,14 @@ export async function compressFile(inputFile: File, quality: number = 1): Promis
   const sanitisedFileName = inputFile.name.replaceAll(/['"[\]{}]/g, '_');
   const file = new File([inputFile], sanitisedFileName, { type: inputFile.type });
   if (file.size < maxSize) return file; // if below 25 MB, don't do anything
+  const type = imageTypes.WEBP;
   const res = await compressImage(file, {
     quality,
-    type: imageTypes.JPEG,
+    type,
   });
   const lowerQuality = quality - 0.01; // NoSonar reduce quality by 1%;
   if (res.size > maxSize) return await compressFile(file, lowerQuality); // compress original file with lower quality setting to avoid double compression
   const fileName = file.name.split('.').slice(0, -1).join('.');
-  const newFileName = fileName + '-min.jpg';
-  return new File([res], newFileName, { type: imageTypes.JPEG });
+  const newFileName = `${fileName}-min.${type}`;
+  return new File([res], newFileName, { type });
 }
