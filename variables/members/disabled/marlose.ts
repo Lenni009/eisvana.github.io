ec1bde289d3cc1bf2a0c202d90aec63e11d0ea81
Lenni009 <guentherhanslphdyt@gmail.com>
2024-03-22 10:57:02 +0100

    disable placeholders

--- variables/members/disabled/marlose.ts
+++ variables/members/disabled/marlose.ts
@@ -1,4 +1,4 @@
-import type { EisvanaMember } from '../../types/member';
+import type { EisvanaMember } from '../../../types/member';
 
 const eisvanaMember: EisvanaMember = {
   avatar: 'https://cdn.discordapp.com/avatars/341629282658942976/c9d1d9923f1ec9932f8e692807be44f2.webp',
--- variables/members/disabled/yuca.ts
+++ variables/members/disabled/yuca.ts
@@ -1,4 +1,4 @@
-import type { EisvanaMember } from '../../types/member';
+import type { EisvanaMember } from '../../../types/member';
 
 const eisvanaMember: EisvanaMember = {
   avatar: 'https://cdn.discordapp.com/avatars/713772682205986886/a3fe5e45a57423b90a3bd770334e37ff.webp',
