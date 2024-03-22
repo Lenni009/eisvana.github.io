9c63a5c64c14a67670118b11e18dedf2ba6da588
Lenni009 <guentherhanslphdyt@gmail.com>
2024-03-22 13:47:04 +0100

    reorganise sidebar

--- .vitepress/sidebars/sidebarAbout.ts
+++ .vitepress/sidebars/sidebarAbout.ts
@@ -7,26 +7,35 @@ export function sidebarAbout(route: string): DefaultTheme.SidebarItem[] {
   const sidebarItems: DefaultTheme.SidebarItem[] = [
     {
       text: 'About Us',
-      collapsed: false,
       items: [
-        { text: 'The Civ', link: '/civ' },
+        { text: 'About Eisvana', link: '/' },
         { text: 'Discord', link: '/discord' },
         { text: 'How to Join', link: '/joining' },
         { text: 'News', link: '/news' },
-        { text: 'Departments', link: '/departments' },
-        { text: 'Rules', link: '/rules' },
-        { text: 'Naming Guidelines', link: '/naming' },
         { text: 'FAQ', link: '/faq' },
-        // { text: 'Medals', link: '/medals' },
-        { text: 'Community', link: '/community/' },
       ],
     },
-    // {
-    //   items: [
-    //     // { text: 'Government Services', base: '/services', link: '/' },
-    //     // { text: 'Blogs', base: '/blog', link: '/' },
-    //   ],
-    // },
+    {
+      text: 'Guidelines',
+      items: [
+        { text: 'Rules', link: '/rules' },
+        { text: 'Naming Guidelines', link: '/naming' },
+      ],
+    },
+    {
+      text: 'Participating',
+      items: [
+        { text: 'Departments', link: '/departments' },
+        { text: 'Medals', link: '/medals' },
+      ],
+    },
+    {
+      text: 'Community',
+      items: [
+        { text: 'Community', link: '/community/' },
+        { text: 'Blogs', base: '/blog', link: '/' },
+      ],
+    },
   ];
 
   return prefixRoute(sidebarItems, route);
