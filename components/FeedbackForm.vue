48685938d2e3f3c28697b255bfb22a93140136aa
Lenni009 <guentherhanslphdyt@gmail.com>
2024-03-23 15:09:18 +0100

    wrap form in form element

--- components/FeedbackForm.vue
+++ components/FeedbackForm.vue
@@ -59,148 +59,150 @@ const formData = computed(() => buildFeedbackFormData(feedbackData));
 </script>
 
 <template>
-  <PicoStyle>
-    <div class="questions">
-      <article>
-        <p class="title">How would you rate your overall experience in Eisvana?</p>
-        <Scale
-          v-model="experience"
-          high="Good"
-          low="Bad"
-          name="experience"
-        />
-      </article>
+  <form @submit.prevent>
+    <PicoStyle>
+      <div class="questions">
+        <article>
+          <p class="title">How would you rate your overall experience in Eisvana?</p>
+          <Scale
+            v-model="experience"
+            high="Good"
+            low="Bad"
+            name="experience"
+          />
+        </article>
 
-      <article>
-        <p class="title">How did you find Eisvana?</p>
-        <MultipleChoice
-          v-model="find"
-          :items="findItems"
-          :other="true"
-          name="find"
-        />
-      </article>
+        <article>
+          <p class="title">How did you find Eisvana?</p>
+          <MultipleChoice
+            v-model="find"
+            :items="findItems"
+            :other="true"
+            name="find"
+          />
+        </article>
 
-      <article>
-        <p class="title">What do you enjoy doing in game?</p>
-        <p class="explanation">E.g. building, exploration, documentation, quests, Sentinel fights, PvP, etc.</p>
-        <input
-          v-model="enjoy"
-          type="text"
-        />
-      </article>
+        <article>
+          <p class="title">What do you enjoy doing in game?</p>
+          <p class="explanation">E.g. building, exploration, documentation, quests, Sentinel fights, PvP, etc.</p>
+          <input
+            v-model="enjoy"
+            type="text"
+          />
+        </article>
 
-      <article>
-        <p class="title">What does Eisvana do well?</p>
-        <input
-          v-model="good"
-          type="text"
-        />
-      </article>
+        <article>
+          <p class="title">What does Eisvana do well?</p>
+          <input
+            v-model="good"
+            type="text"
+          />
+        </article>
 
-      <article>
-        <p class="title">What could be improved?</p>
-        <input
-          v-model="bad"
-          type="text"
-        />
-      </article>
+        <article>
+          <p class="title">What could be improved?</p>
+          <input
+            v-model="bad"
+            type="text"
+          />
+        </article>
 
-      <article>
-        <p class="title">What kind of project would you like to participate in?</p>
-        <p>What are you passionate about and would like to see us do?</p>
-        <input
-          v-model="project"
-          type="text"
-        />
-      </article>
+        <article>
+          <p class="title">What kind of project would you like to participate in?</p>
+          <p>What are you passionate about and would like to see us do?</p>
+          <input
+            v-model="project"
+            type="text"
+          />
+        </article>
 
-      <article>
-        <p class="title">Do you know about Eisvana's departments?</p>
-        <p>E.g. EBC, Wiki Scholars</p>
-        <MultipleChoice
-          v-model="departments"
-          :items="departmentItems"
-          name="departments"
-        />
-      </article>
+        <article>
+          <p class="title">Do you know about Eisvana's departments?</p>
+          <p>E.g. EBC, Wiki Scholars</p>
+          <MultipleChoice
+            v-model="departments"
+            :items="departmentItems"
+            name="departments"
+          />
+        </article>
 
-      <article>
-        <p class="title">How much do you agree with the following statements?</p>
-      </article>
+        <article>
+          <p class="title">How much do you agree with the following statements?</p>
+        </article>
 
-      <article>
-        <p class="title">I understand Eisvana's organisational and leadership structure</p>
-        <Scale
-          v-model="structure"
-          high="Agree"
-          low="Disagree"
-          name="structure"
-        />
-      </article>
+        <article>
+          <p class="title">I understand Eisvana's organisational and leadership structure</p>
+          <Scale
+            v-model="structure"
+            high="Agree"
+            low="Disagree"
+            name="structure"
+          />
+        </article>
 
-      <article>
-        <p class="title">I feel "at home" in Eisvana</p>
-        <Scale
-          v-model="home"
-          high="Agree"
-          low="Disagree"
-          name="home"
-        />
-      </article>
+        <article>
+          <p class="title">I feel "at home" in Eisvana</p>
+          <Scale
+            v-model="home"
+            high="Agree"
+            low="Disagree"
+            name="home"
+          />
+        </article>
 
-      <article>
-        <p class="title">Eisvana's Discord server is organised</p>
-        <Scale
-          v-model="server"
-          high="Agree"
-          low="Disagree"
-          name="server"
-        />
-      </article>
+        <article>
+          <p class="title">Eisvana's Discord server is organised</p>
+          <Scale
+            v-model="server"
+            high="Agree"
+            low="Disagree"
+            name="server"
+          />
+        </article>
 
-      <article>
-        <p class="title">Eisvana's leadership is committed to transparency</p>
-        <Scale
-          v-model="transparency"
-          high="Agree"
-          low="Disagree"
-          name="transparency"
-        />
-      </article>
+        <article>
+          <p class="title">Eisvana's leadership is committed to transparency</p>
+          <Scale
+            v-model="transparency"
+            high="Agree"
+            low="Disagree"
+            name="transparency"
+          />
+        </article>
 
-      <article>
-        <p class="title">Eisvana's leadership values Eisvana's citizens</p>
-        <Scale
-          v-model="citizenValue"
-          high="Agree"
-          low="Disagree"
-          name="citizen-value"
-        />
-      </article>
+        <article>
+          <p class="title">Eisvana's leadership values Eisvana's citizens</p>
+          <Scale
+            v-model="citizenValue"
+            high="Agree"
+            low="Disagree"
+            name="citizen-value"
+          />
+        </article>
 
-      <article>
-        <p class="title">I am well informed about latest developments and projects</p>
-        <Scale
-          v-model="news"
-          high="Agree"
-          low="Disagree"
-          name="news"
-        />
-      </article>
+        <article>
+          <p class="title">I am well informed about latest developments and projects</p>
+          <Scale
+            v-model="news"
+            high="Agree"
+            low="Disagree"
+            name="news"
+          />
+        </article>
 
-      <article>
-        <p class="title">Do you have any other feedback?</p>
-        <textarea v-model="otherFeedback"></textarea>
-      </article>
-    </div>
+        <article>
+          <p class="title">Do you have any other feedback?</p>
+          <textarea v-model="otherFeedback"></textarea>
+        </article>
+      </div>
 
-    <SubmitButton
-      v-if="isFilledOut"
-      :webhook
-      :form-data-array="[formData]"
-    />
-  </PicoStyle>
+      <SubmitButton
+        v-if="isFilledOut"
+        :webhook
+        :form-data-array="[formData]"
+      />
+    </PicoStyle>
+  </form>
 </template>
 
 <style scoped lang="scss">
