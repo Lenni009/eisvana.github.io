08280c265c7f444e6d4715fa7f8e5bd0f7f903dd
Lenni009 <guentherhanslphdyt@gmail.com>
2024-03-23 13:14:57 +0100

    add type guards

--- logic/formHelpers.ts
+++ logic/formHelpers.ts
@@ -1,9 +1,10 @@
 import { type Ref } from 'vue';
 
-export function deselect(e: Event, model: Ref) {
+export function deselect(e: Event, model: Ref<string | number | undefined>) {
   const { target } = e;
   if (!(target instanceof HTMLInputElement)) return;
-  if (model.value === target.value) {
+  const value = typeof model.value === 'number' ? parseInt(target.value) : target.value;
+  if (model.value === value) {
     model.value = undefined; // NoSonar deselect everything if the currently selected item is clicked
   }
 }
