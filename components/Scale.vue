251b5caacabb9e2645168a6ada57bc6aa8481038
Lenni009 <guentherhanslphdyt@gmail.com>
2024-03-23 12:14:38 +0100

    working scale component

---
+++ components/Scale.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+defineProps<{
+  name: string;
+  low: string;
+  high: string;
+}>();
+
+const model = defineModel({ type: Number });
+</script>
+
+<template>
+  <div class="scale-wrapper">
+    <div>{{ low }}</div>
+    <div
+      v-for="n in 5"
+      class="scale-item"
+    >
+      <label :for="`${name}${n}`">{{ n }}</label>
+      <input
+        v-model="model"
+        :id="`${name}-${n}`"
+        :name
+        :value="n"
+        type="radio"
+      />
+    </div>
+    <div>{{ high }}</div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.scale-wrapper {
+  display: flex;
+  gap: 0.5rem;
+  align-items: flex-end;
+  justify-content: space-between;
+
+  .scale-item {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    input {
+      margin: 0;
+    }
+  }
+}
+</style>
