5504a9daab42e2187d5499864dfb2787a340c5f0
Lenni009 <guentherhanslphdyt@gmail.com>
2024-03-29 12:47:26 +0100

    add Lianeras

---
+++ variables/members/3_lianeras.ts
@@ -0,0 +1,12 @@
+// import { getMedal } from '../../logic/getMedal';
+import type { EisvanaMember } from '../../types/member';
+
+const eisvanaMember: EisvanaMember = {
+  avatar: 'lianeras.png',
+  name: 'Lianeras',
+  desc: 'Microbiologist by day, Builder by night',
+  // medals: [getMedal('warn', 32), getMedal('eisvana'), getMedal('test1'), getMedal('test2')],
+  pageLink: './Lianeras',
+};
+
+export default eisvanaMember;
