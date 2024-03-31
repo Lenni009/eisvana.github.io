024c298b02f7a6f2b2dec71a6ea3f114d7a63e0d
Lenni009 <guentherhanslphdyt@gmail.com>
2024-03-31 17:25:51 +0200

    fix file extension

--- logic/compressImage.ts
+++ logic/compressImage.ts
@@ -7,6 +7,7 @@ export async function compressFile(inputFile: File, quality: number = 1): Promis
   const file = new File([inputFile], sanitisedFileName, { type: inputFile.type });
   if (file.size < maxSize) return file; // if below 25 MB, don't do anything
   const type = imageTypes.WEBP;
+  const newFileExtension = type.split('/').at(-1);
   const res = await compressImage(file, {
     quality,
     type,
@@ -14,6 +15,6 @@ export async function compressFile(inputFile: File, quality: number = 1): Promis
   const lowerQuality = quality - 0.01; // NoSonar reduce quality by 1%;
   if (res.size > maxSize) return await compressFile(file, lowerQuality); // compress original file with lower quality setting to avoid double compression
   const fileName = file.name.split('.').slice(0, -1).join('.');
-  const newFileName = `${fileName}-min.${type}`;
+  const newFileName = `${fileName}-min.${newFileExtension}`;
   return new File([res], newFileName, { type });
 }
