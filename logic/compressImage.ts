3d23fcfcc7d97df4055553bf9551c86a3a8ec0b4
Lenni009 <guentherhanslphdyt@gmail.com>
2024-09-10 11:21:57 +0200

    set Discord upload limit to 10MB

--- logic/compressImage.ts
+++ logic/compressImage.ts
@@ -1,11 +1,11 @@
-import { maxSize } from '../variables/fileCompression';
+import { maxFileSize } from '../variables/fileCompression';
 import { imageTypes, compressImage } from 'simple-image-compressor';
 import { escapeFileName } from './fileNameEscape';
 
 export async function compressFile(inputFile: File, quality: number = 1): Promise<File> {
   const sanitisedFileName = escapeFileName(inputFile.name);
   const file = new File([inputFile], sanitisedFileName, { type: inputFile.type });
-  if (file.size < maxSize) return file; // if below 2 MB, don't do anything
+  if (file.size < maxFileSize) return file; // if below 2 MB, don't do anything
   const type = imageTypes.WEBP;
   const newFileExtension = type.split('/').at(-1);
   const res = await compressImage(file, {
@@ -13,7 +13,7 @@ export async function compressFile(inputFile: File, quality: number = 1): Promis
     type,
   });
   const lowerQuality = quality - 0.01; // NoSonar reduce quality by 1%;
-  if (res.size > maxSize) return await compressFile(file, lowerQuality); // compress original file with lower quality setting to avoid double compression
+  if (res.size > maxFileSize) return await compressFile(file, lowerQuality); // compress original file with lower quality setting to avoid double compression
   const fileName = file.name.split('.').slice(0, -1).join('.');
   const newFileName = `${fileName}-min.${newFileExtension}`;
   return new File([res], newFileName, { type });
--- variables/fileCompression.ts
+++ variables/fileCompression.ts
@@ -1,1 +1,4 @@
-export const maxSize = 2000000; // 2MB, to keep page load speeds fast
+export const maxFilesPerMessage = 10; // maximum amount of files in one Discord message
+export const maxFileSize  = 2000000; // 2MB, to keep page load speeds fast
+
+export const maxMessageSize = 10000000; // 10MB, Discord upload limit
